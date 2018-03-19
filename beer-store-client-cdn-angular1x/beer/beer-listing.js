38a6b976b991eb783bb63f4e63acfb2000e264cb
sombriks <sombriks@gmail.com>
2018-03-19 15:51:28 -0300

    now the old angular client has the same partial funcionality seen on the other client.

--- beer-store-client-cdn-angular1x/beer/beer-listing.js
+++ beer-store-client-cdn-angular1x/beer/beer-listing.js
@@ -4,9 +4,15 @@ class BeerListing {
   constructor(BeerService) {
     console.log("there!");
     this.BeerService = BeerService;
-    BeerService.list().then(ret => {
+    this.query = { search: "", page: 1, pageSize: 10 };
+    this.list();
+  }
+
+  list(p) {
+    console.log(this.query);
+    if (p) this.query.page += p;
+    this.BeerService.list(this.query).then(ret => {
       this.beers = ret.data;
-      console.log(this.beers);
     });
   }
 }
