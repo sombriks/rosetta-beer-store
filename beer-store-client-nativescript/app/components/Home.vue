6c5679b12e5eae78db68aa95e8fe2703cdda27ed
Leonardo Silveira <sombriks@gmail.com>
2019-11-30 15:11:21 -0300

    toying with layouts

--- beer-store-client-nativescript/app/components/Home.vue
+++ beer-store-client-nativescript/app/components/Home.vue
@@ -1,41 +1,43 @@
 <template>
-    <Page>
-        <ActionBar>
-            <Label text="Beer Store"></Label>
-        </ActionBar>
-
-        <GridLayout columns="*,auto">
-            <TextField hint="Search beer" v-model="textFieldValue" />
-        </GridLayout>
-    </Page>
+  <Page>
+    <ActionBar>
+      <Label text="Beer Store"></Label>
+    </ActionBar>
+    <StackLayout>
+      <!-- https://nativescript-vue.org/en/docs/elements/layouts/stack-layout/ -->
+      <TextField hint="Search beer" v-model="textFieldValue" />
+      <Button text="Search" @tap="doSearch" />
+    </StackLayout>
+  </Page>
 </template>
 
 <script>
-    export default {
-        computed: {
-            message() {
-                return "Blank {N}-Vue app";
-            }
-        },
-        data(){
-            return {
-                textFieldValue:""
-            }
-        }
+export default {
+  name: "home",
+  data() {
+    return {
+      textFieldValue: ""
     };
+  },
+  methods: {
+    doSearch() {
+      console.log(this.textFieldValue);
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    @import '~@nativescript/theme/scss/variables/blue';
+@import "~@nativescript/theme/scss/variables/blue";
 
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
+// Custom styles
+.fas {
+  @include colorize($color: accent);
+}
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+.info {
+  font-size: 20;
+  horizontal-align: center;
+  vertical-align: center;
+}
 </style>
