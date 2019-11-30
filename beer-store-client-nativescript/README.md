67bc2dcb3ca2b19f06457d81b9653ced437b656d
Leonardo Silveira <sombriks@gmail.com>
2019-11-30 14:53:58 -0300

    nativescript client

---
+++ beer-store-client-nativescript/README.md
@@ -0,0 +1,25 @@
+# beer-store-client-nativescript
+
+Nativescript "hybrid" app to consume the beer service
+
+## requirements
+
+- Node latest lts
+- Java 8+
+- Android platform SDK
+- Android studio
+- At least one virtual device or the real thing
+
+## how to run this
+
+First install nativescript command line tool
+
+```bash
+npm --unsafe-perm install -g nativescript
+```
+
+Then enter this folder and type `tns run android`.
+
+The nativescript client will perform lots of checks and tell you what must be
+made in order to rock the boat. Be patient and set variables, download sdks,
+create virtual devices and so on.
---
+++ beer-store-client-nativescript/app/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Beer Store"></Label>
+        </ActionBar>
+
+        <GridLayout columns="*,auto">
+            <TextField hint="Search beer" v-model="textFieldValue" />
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+    export default {
+        computed: {
+            message() {
+                return "Blank {N}-Vue app";
+            }
+        },
+        data(){
+            return {
+                textFieldValue:""
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    @import '~@nativescript/theme/scss/variables/blue';
+
+    // Custom styles
+    .fas {
+        @include colorize($color: accent);
+    }
+
+    .info {
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+</style>
