c55cb2f5b2b1761fc3dde565dd518058a497f072
sombriks <sombriks@gmail.com>
2022-04-02 21:06:37 -0300

    another service. a nice one

--- beer-store-service-kotlin-javalin-ktorm-liquibase/src/main/kotlin/beer/store/Main.kt
+++ beer-store-service-kotlin-javalin-ktorm-liquibase/src/main/kotlin/beer/store/Main.kt
@@ -4,17 +4,30 @@ import beer.store.config.Db
 import beer.store.controllers.BeerController
 import beer.store.controllers.MediaContoller
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import io.javalin.Javalin
+import io.javalin.plugin.json.JavalinJackson
+import org.ktorm.jackson.KtormModule
 
 // service entry point
 fun main() {
+    // ensure database is sane
     Db.migrate()
 
+    // custom config to make ktor and jackson behave
+    val mapper = ObjectMapper()
+    mapper.registerModule(JavaTimeModule())
+    mapper.registerModule(KotlinModule.Builder().build())
+    mapper.registerModule(KtormModule())
+
+    // spin up app
     val app = Javalin.create {
-//        it.jsonMapper(ObjectMapper())
+        it.jsonMapper(JavalinJackson(mapper))
     }.start(3000)
 
-
+    // routes
     app.get(BeerController.BEER_LIST) { BeerController.list(it) }
     app.get(BeerController.BEER_FIND) { BeerController.find(it) }
 
