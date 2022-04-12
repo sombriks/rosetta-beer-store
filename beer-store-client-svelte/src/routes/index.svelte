f096c13d7dcd2911ff48bb5cbe03dd43c7884ea9
sombriks <sombriks@gmail.com>
2022-04-11 21:03:10 -0300

    small fix

--- beer-store-client-svelte/src/routes/index.svelte
+++ beer-store-client-svelte/src/routes/index.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	import { listBeers } from '$lib/api';
 
 	import Listing from '$lib/listing.svelte';
 	import Search from '$lib/search.svelte';
-  
+
 	let beers = [];
 
 	async function doSearch(e) {
 		beers = await listBeers(e.detail);
 	}
+
+	onMount(() => {
+		doSearch({ detail: { search: '', page: 1, pageSize: 10 } });
+	});
 </script>
 
 <h1>Beer store</h1>
